aa591f9225c6879acc13e878bec3c1a6051c505c
Keegan Hinton <khinton386@gmail.com>
2024-08-25 01:34:30 -0400

    created a function to render license badge

--- Develop /utils/generateMarkdown.js
+++ Develop /utils/generateMarkdown.js
@@ -1,6 +1,26 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'MIT') {
+    return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`;
+  } else if (license === 'GNU AGPLv3') {
+    return `![License: GNU AGPLv3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)`;
+  } else if (license === 'GNU GPLv3') {
+    return `![License: GNU GPLv3](https://img.shields.io/badge/License-GPLv3-blue.svg)`;
+  } else if (license === 'GNU LGPLv3') {
+    return `![License: GNU LGPLv3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)`;
+  } else if (license ==='Mozilla') {
+    return '![License: Mozilla](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)';
+  }else if (license ==='Boost') {
+    return '!License: Boost](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)';
+  }else if (license ==='Unlicense') {
+    return '!License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)';
+  }else if (license ==='Apache') {
+    return '!License: Apache](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
+  }else{
+    return '![License: None](https://img.shields.io/badge/License-None-lightgrey.svg)';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
